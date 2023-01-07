6b6de19d4e08d905af5dc80060216c54f75748fe
Rond <rond.nunes@gmail.com>
2023-01-07 16:16:56 -0300

    fix loop

--- test.py
+++ test.py
@@ -5,20 +5,28 @@ URL = "https://pt.annas-archive.org/search?q=deleuze"
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
-#results = soup.find(id="mb-4");
 results = soup.find_all(class_="h-[125]")
 
 # eu consigo acessar os resultados pelo results[], printando nome do autor
-print(results[0].find_all("div")[7].get_text())
-print(results[1].find_all("div")[7].get_text())
+#print(results[0].find_all("div")[7].get_text())
+#print(results[1].find_all("div")[7].get_text())
 
 # mas não consigo rodar dentro da lista e organizar os itens pra limpá-los, dá o erro:  IndexError: list index out of range
 books = []
+limite = 11
 
 for book in results:
-    new_book = [book.find_all("div")[7].get_text(),
+    if(limite==0):
+        break
+    else:
+        new_book = [book.find_all("div")[7].get_text(),
             book.find_all("div")[5].get_text(), 
             book.find_all("div")[4].get_text(),
             book.find_all("div")[6].get_text(),
-            book.find("a")['href']]
-    books.append(new_book)
+            "https://pt.annas-archive.org"+book.find("a")['href']]
+        books.append(new_book)
+        limite -= 1
+for livro in books:
+    for campo in livro:
+        print(f'{campo}\n')
+    print('\n')
