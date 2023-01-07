ebde6c7ad26664c63a2559581c1ebc731a5c18ea
Rond <rond.nunes@gmail.com>
2023-01-07 20:00:32 -0300

    limits and presentation

--- annas_archive.py
+++ annas_archive.py
@@ -1,8 +1,7 @@
 import requests
-import sys
 from bs4 import BeautifulSoup
 
-def search(input):
+def search(input, limit=10):
     URL = "https://pt.annas-archive.org/search?q="+input
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, "html.parser")
@@ -11,22 +10,13 @@ def search(input):
 
     for book in results:
         try:
-            new_book = [book.find_all("div")[7].get_text(),
-                book.find_all("div")[5].get_text(), 
-                book.find_all("div")[4].get_text(),
-                book.find_all("div")[6].get_text(),
-                "https://pt.annas-archive.org"+book.find("a")['href']]
-            books.append(new_book)
+            if(len(books)<int(limit)):
+                new_book = [book.find_all("div")[7].get_text(),
+                    book.find_all("div")[5].get_text(), 
+                    book.find_all("div")[4].get_text(),
+                    book.find_all("div")[6].get_text(),
+                    "https://pt.annas-archive.org"+book.find("a")['href']]
+                books.append(new_book)
         except IndexError:
             pass
     return books
-
-try:
-    books = search(sys.argv[1])
-except IndexError:
-    books = search(input("Author or book name:\n"))
-    
-for book in books:
-    for field in book:
-        print(f'{field}\n')
-    print('\n')
--- use.py
+++ use.py
@@ -1,12 +1,13 @@
 import annas_archive as anna
 import sys
 
+limit = 5
 try:
-    books = anna.search(sys.argv[1])
+    books = anna.search(sys.argv[1], sys.argv[2])
 except IndexError:
-    books = anna.search(input("Author or book name:\n"))
+    books = anna.search(input("Author or book name:\n"), limit)
     
 for book in books:
-    for field in book:
-        print(f'{field}\n')
+    print(f'{book[1]}, de {book[0]}. {book[4]}')
+# {book[2]}: 2, {book[3]}: 3,
     print('\n')
