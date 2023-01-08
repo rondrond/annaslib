e126509ec3fc6ac52df391e320acc9faa16f9b23
Rond <rond.nunes@gmail.com>
2023-01-08 15:34:52 -0300

    replies only to hashtagged mentions

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ from mastodon import StreamListener
 import annas_archive as anna
 import re
 botat = '@bibliotecaria'
+hashtag ="pfv"
 
 mastodon = Mastodon(
     access_token = '../token.secret',
@@ -15,17 +16,22 @@ def remove_html_tags(text):
 class Listener(StreamListener):
         def on_notification(self, notification):
             book_name = remove_html_tags(notification["status"]["content"]).replace(botat, '')
-            if((notification["type"] == "mention") and (len(book_name)<30)):
-                print(f'Nova menção de @{notification["account"]["acct"]}, pedindo {book_name}\n')
-                books = anna.search(book_name, limit=3)
-                print(f'Livro solicitado: {book_name}, minhas sugestões são:')
-                suggestions = ""
-                for book in books:
-                    suggestions = suggestions + book[1][:50]+", de "+book[0][:50]+". "+book[4]+"\n"
-                suggestions = "@"+notification["account"]["acct"]+" Oi! Essas são minhas sugestões:\n"+suggestions
-                mastodon.status_post(suggestions, in_reply_to_id=notification["status"]["id"])
-                print(suggestions)
-                
-            mastodon.notifications_dismiss(notification["id"])
+           
+            if((notification["type"] == "mention") and (len(notification["status"]["tags"])>0)):
+                clean_tags = []
+                for tag in notification["status"]["tags"]:
+                    clean_tags.append(tag["name"].lower())
+                if(hashtag in clean_tags):    
+                    print(f'Nova menção de @{notification["account"]["acct"]}, pedindo {book_name}\n')
+                    books = anna.search(book_name, limit=3)
+                    print(f'Livro solicitado: {book_name}, minhas sugestões são:')
+                    suggestions = ""
+                    for book in books:
+                        suggestions = suggestions + book[1][:50]+", de "+book[0][:50]+". "+book[4]+"\n"
+                    suggestions = "@"+notification["account"]["acct"]+" Oi! Essas são minhas sugestões:\n"+suggestions
+                    mastodon.status_post(suggestions, in_reply_to_id=notification["status"]["id"])
+                    print(suggestions)
+                    
+                mastodon.notifications_dismiss(notification["id"])
 
 mastodon.stream_user(Listener())
