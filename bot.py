208860b453fb6af320099d617fc62eca41a074ab
Rond <rond.nunes@gmail.com>
2023-01-08 15:07:25 -0300

    stable; fixed errors. todo: reply only with hashtag

--- bot.py
+++ bot.py
@@ -2,18 +2,21 @@ from mastodon import Mastodon
 from mastodon import StreamListener
 from bs4 import BeautifulSoup
 import annas_archive as anna
-
+import re
 botat = '@bibliotecaria'
 
 mastodon = Mastodon(
     access_token = '../token.secret',
     api_base_url = 'https://botsin.space/'
 )
+def remove_html_tags(text):
+    clean = re.compile('<.*?>')
+    return re.sub(clean, '', text)
 
 class Listener(StreamListener):
         def on_notification(self, notification):
-            if(notification["type"] == "mention"):
-                book_name = BeautifulSoup(notification["status"]["content"], features="lxml").get_text().strip(botat)
+            book_name = remove_html_tags(notification["status"]["content"]).replace(botat, '')
+            if((notification["type"] == "mention") and (len(book_name)<30)):
                 print(f'Nova menção de @{notification["account"]["acct"]}, pedindo {book_name}\n')
                 books = anna.search(book_name, limit=3)
                 print(f'Livro solicitado: {book_name}, minhas sugestões são:')
@@ -23,6 +26,11 @@ class Listener(StreamListener):
                 suggestions = "@"+notification["account"]["acct"]+" Oi! Essas são minhas sugestões:\n"+suggestions
                 mastodon.status_post(suggestions, in_reply_to_id=notification["status"]["id"])
                 print(suggestions)
-                mastodon.notifications_dismiss(notification["id"])
+                
+#            elif((notification["type"] == follow)):
+#                print(f'Pedido de seguir de {notification["account"]["acct"]}, seguindo de volta!')
+#                mastodon.follows(notification["account"]["acct"])
+            
+            mastodon.notifications_dismiss(notification["id"])
 
 mastodon.stream_user(Listener())
