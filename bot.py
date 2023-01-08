29a39f1f15fcd2a874a99ea9ec38a41bebf52c53
Rond <rond.nunes@gmail.com>
2023-01-08 16:11:24 -0300

    fixed errors

--- bot.py
+++ bot.py
@@ -19,30 +19,30 @@ class Listener(StreamListener):
             if(notification["type"] == "mention"):
                 book_name = remove_html_tags(notification["status"]["content"]).lower().replace(botat, '')
            
-            if((notification["type"] == "mention") and (len(notification["status"]["tags"])>0)):
-                clean_tags = []
-                for tag in notification["status"]["tags"]:
-                    clean_tags.append(tag["name"].lower())
-                    book_name = book_name.replace(tag["name"], '')
-                book_name = book_name.replace('#', '')
+                if(len(notification["status"]["tags"])>0):
+                    clean_tags = []
+                    for tag in notification["status"]["tags"]:
+                        clean_tags.append(tag["name"].lower())
+                        book_name = book_name.replace(tag["name"], '')
+                    book_name = book_name.replace('#', '')
 
-                if(hashtag in clean_tags):    
-                    print(f'Nova menção de @{notification["account"]["acct"]}, pedindo {book_name}\n')
-                    books = anna.search(book_name, limit=3)
-                    print(f'Livro solicitado: {book_name}, minhas sugestões são:')
-                    suggestions = ""
-                    for book in books:
-                        suggestions = suggestions + book[1][:50]+", de "+book[0][:50]+". "+book[4]+"\n"
-                    suggestions = "@"+notification["account"]["acct"]+" Oi! Essas são minhas sugestões:\n"+suggestions
-                    mastodon.status_post(suggestions, in_reply_to_id=notification["status"]["id"])
-                    print(suggestions)
+                    if(hashtag in clean_tags):    
+                        print(f'Nova menção de @{notification["account"]["acct"]}, pedindo {book_name}\n')
+                        books = anna.search(book_name, limit=3)
+                        print(f'Livro solicitado: {book_name}, minhas sugestões são:')
+                        suggestions = ""
+                        for book in books:
+                            suggestions = suggestions + book[1][:50]+", de "+book[0][:50]+". "+book[4]+"\n"
+                        suggestions = "@"+notification["account"]["acct"]+" Oi! Essas são minhas sugestões:\n"+suggestions
+                        mastodon.status_post(suggestions, in_reply_to_id=notification["status"]["id"])
+                        print(suggestions)
 
-            elif((notification["type"] == "mention") and (book_name.__contains__(thanks))):
-                msg = "@"+notification["account"]["acct"]+" eu que agradeço, flor!"
-                mastodon.status_post(msg, in_reply_to_id=notification["status"]["id"])
-                print(f'Agradeci a {notification["account"]["acct"]}')
+                elif(book_name.__contains__(thanks)):
+                    msg = "@"+notification["account"]["acct"]+" eu que agradeço, flor!"
+                    mastodon.status_post(msg, in_reply_to_id=notification["status"]["id"])
+                    print(f'Agradeci a {notification["account"]["acct"]}')
 
-            mastodon.notifications_dismiss(notification["id"])
+                mastodon.notifications_dismiss(notification["id"])
 
 
 mastodon.stream_user(Listener())
