22f0b3c65a4a5ba70532b16658bf329eb28a0e04
Rond <rond.nunes@gmail.com>
2023-01-11 09:06:58 -0300

    style

--- annas_archive.py
+++ annas_archive.py
@@ -3,17 +3,17 @@ from bs4 import BeautifulSoup
 from isbnlib import get_canonical_isbn
 
 
-def search(input, limit=5):
-    if (not get_canonical_isbn(input)):
-        URL = "https://pt.annas-archive.org/search?q="+input
-        page = requests.get(URL)
+def search(user_input, limit=5):
+    if not get_canonical_isbn(user_input):
+        annas_url = "https://pt.annas-archive.org/search?q="+user_input
+        page = requests.get(annas_url, timeout=2)
         soup = BeautifulSoup(page.content, "html.parser")
         results = soup.find_all(class_="h-[125]")
         books = []
 
         for book in results:
             try:
-                if (len(books) < int(limit)):
+                if len(books) < int(limit):
                     new_book = [book.find_all("div")[7].get_text(),
                                 book.find_all("div")[5].get_text(),
                                 book.find_all("div")[4].get_text(),
@@ -24,15 +24,16 @@ def search(input, limit=5):
                 pass
         return books
     else:
-        URL = "https://pt.annas-archive.org/isbn/"+get_canonical_isbn(input)
-        page = requests.get(URL)
+        annas_url = "https://pt.annas-archive.org/isbn/" + \
+            get_canonical_isbn(user_input)
+        page = requests.get(annas_url, timeout=2)
         soup = BeautifulSoup(page.content, "html.parser")
-        if (len(soup.find_all('h2', {'class': 'mt-12 mb-1 text-3xl font-bold'})) == 0):
+        if len(soup.find_all('h2', {'class': 'mt-12 mb-1 text-3xl font-bold'})) == 0:
             obj = []
             return obj
         else:
             results = soup.find_all('a', {'class': 'custom-a'})[1]
-            # results['href'] / url
+            # results['href'] / annas_url
             details = results.find_all('div', {'class': 'truncate'})
             # details[0].get_text() / format
             # details[1].get_text() / nome do livro
@@ -41,4 +42,3 @@ def search(input, limit=5):
             new_book = [[details[3].get_text(), details[1].get_text(
             ), details[0].get_text(), "https://pt.annas-archive.org"+results['href']]]
             return new_book
-            
--- bot.py
+++ bot.py
@@ -1,13 +1,13 @@
+import re
+import time
 from mastodon import Mastodon
 from mastodon import StreamListener
 import annas_archive as anna
-import re
 import urllib3
-import time
 
-botat = '@bibliotecaria'
-hashtag = "pfv"
-thanks = "brigad"
+BOT_AT = '@bibliotecaria'
+HASHTAG = "pfv"
+THANKS = "brigad"
 
 mastodon = Mastodon(access_token='../token.secret',
                     api_base_url='https://botsin.space/')
@@ -20,29 +20,29 @@ def remove_html_tags(text):
 
 class Listener(StreamListener):
     def on_notification(self, notification):
-        if (notification["type"] == "mention"):
+        if notification["type"] == "mention":
             book_name = remove_html_tags(
-                notification["status"]["content"]).lower().replace(botat, '')
+                notification["status"]["content"]).lower().replace(BOT_AT, '')
 
-            if (len(notification["status"]["tags"]) > 0):
+            if len(notification["status"]["tags"]) > 0:
                 clean_tags = []
                 for tag in notification["status"]["tags"]:
                     clean_tags.append(tag["name"].lower())
                     book_name = book_name.replace(tag["name"], '')
                 book_name = book_name.replace('#', '')
 
-                if (hashtag in clean_tags):
+                if HASHTAG in clean_tags:
                     print(
                         f'Nova menção de @{notification["account"]["acct"]}, pedindo {book_name}\n')
                     books = anna.search(book_name, limit=3)
-                    if (len(books) == 0):
+                    if len(books) == 0:
                         mastodon.status_post("@"+notification["account"]["acct"]+" Oi! Não encontrei nenhum livro :/ Foi mal!",
                                              in_reply_to_id=notification["status"]["id"], visibility='direct')
                         print('Não encontrei nenhum livro')
                     else:
                         suggestions = ""
                         for book in books:
-                            if (len(books) > 1):
+                            if len(books) > 1:
                                 suggestions = suggestions + \
                                     book[1][:50]+", de " + \
                                     book[0][:50]+". "+book[4]+"\n"
@@ -50,7 +50,7 @@ class Listener(StreamListener):
                             else:
                                 suggestions = book[1]+", de " + \
                                     book[0]+". "+book[3]+"\n"
-                        if (("public" in clean_tags)):
+                        if "public" in clean_tags:
                             visib = 'public'
                         else:
                             visib = 'direct'
@@ -61,7 +61,7 @@ class Listener(StreamListener):
                             suggestions, in_reply_to_id=notification["status"]["id"], visibility=visib)
                         print(suggestions)
 
-            elif (book_name.__contains__(thanks)):
+            elif book_name.__contains__(THANKS):
                 msg = "@"+notification["account"]["acct"] + \
                     " eu que agradeço, flor!"
                 mastodon.status_post(
