152b2414758637367727adf52d9f264844e12bfe
Rond <rond.nunes@gmail.com>
2023-01-12 11:17:15 -0300

    fix telegram bot interaction

--- telegram_bot.py
+++ telegram_bot.py
@@ -11,14 +11,14 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user = update.effective_user
 
     await update.message.reply_html(
-        rf"Oi {user.mention_html()}!, boyzera demais? Use o comando /pesquisar com o nome ou o ISBN de um livro que você quer baixar.",
+        rf"Oi {user.mention_html()}!, boyzera demais? Tudo que você digitar será pesquisado. Não é preciso usar nenhum comando. Agora também mostro mais resultados! Os outros comandos são /ajuda e /start, pra iniciar o bot.",
         reply_markup=ForceReply(
             selective=True, input_field_placeholder='/pesquisar '),
     )
 
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text("Uai, quer ajuda com o quê?!")
+    await update.message.reply_text("Uai, quer ajuda com o quê?! Ainda não posso te ajudar em nada. Apenas digite o nome do livro ou ISBN que ele será pesquisado")
 
 
 async def log_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -26,8 +26,8 @@ async def log_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Non
 
 
 async def search_book(update: Update, context) -> None:
-    name = " ".join(context.args)
-    books = anna.search(name, limit=3)
+    name = update.message.text
+    books = anna.search(name, limit=6)
     if len(books) == 0:
         await update.message.reply_text('Não encontrei nenhum livro')
     else:
@@ -35,20 +35,14 @@ async def search_book(update: Update, context) -> None:
         for book in books:
             if len(books) > 1:
                 suggestions = suggestions + \
-                    book[1][:50]+", de "+book[0][:50]+". "+book[4]+"\n"
+                    book[1][:50]+", de "+book[0][:50] + \
+                    " ("+book[2]+"). "+book[4]+"\n\n"
 
             else:
-                suggestions = book[1]+", de "+book[0]+". "+book[3]+"\n"
+                suggestions = book[1]+", de "+book[0] + \
+                    " ("+book[2]+"). "+book[3]+"\n"
         suggestions = "Oi! Essas são minhas sugestões:\n"+suggestions
         await update.message.reply_text(suggestions)
-
-
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    MemePy.MemeGenerator.save_meme_to_disk(
-        'Classy', 'memes', ["Bibliotecaria virtual", update.message.text])
-    await update.message.reply_document(document=open('memes/meme.jpg', 'rb'))
-    await update.message.reply_text("tô anotando tudo, meus advogados entrarão em contato!")
-    print(update.message.text)
 
 
 def main() -> None:
@@ -56,11 +50,10 @@ def main() -> None:
     application = Application.builder().token(api_key).build()
     print('Rodando...')
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("help", help_command))
-    application.add_handler(CommandHandler("pesquisar", search_book))
+    application.add_handler(CommandHandler("ajuda", help_command))
     application.add_handler(CommandHandler("log", log_command))
 
-    application.add_handler(MessageHandler(filters.TEXT, echo))
+    application.add_handler(MessageHandler(filters.TEXT, search_book))
     application.run_polling()
 
 
